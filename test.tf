ca502879524cd8f430b17bf1e900c02ff3e3c5c8
yusuf karatoprak <yusufkaratoprak@yahoo.com>
2021-04-15 21:49:03 +0300

    update

--- test.tf
+++ test.tf
@@ -13,28 +13,3 @@ resource "aws_s3_bucket" "b1" {
   }
 }
 
-data "aws_ami" "ubuntu2" {
-  most_recent = true
-
-  filter {
-    name   = "name2"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {a
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-
-resource "aws_instance" "web2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld-leanscale-2"
-  }
-}
