80c0722bdffd4c8b811347a48f4160c4340ec1a9
yusuf karatoprak <yusufkaratoprak@yahoo.com>
2021-04-16 00:15:53 +0300

    ass provider2.tf

---
+++ provider2.tf
@@ -0,0 +1,40 @@
+#OpenShot Terraform Project
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-leanscale-2"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld-leanscale-2"
+  }
+}
